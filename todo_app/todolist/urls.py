4a1d9a4da86d05e09e17a7759dbdee359026f400
Bituse <slobodanovski.stefan@gmail.com>
2022-03-12 11:41:19 +0100

    Done to login

--- todo_app/todolist/urls.py
+++ todo_app/todolist/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import taskList
+from .views import TaskDelete, TaskUpdate, taskList
+from .views import TaskDetail, TaskCreate, TaskDelete
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', taskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
 ]
